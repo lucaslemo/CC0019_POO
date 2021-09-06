cdd0ba75f302e0bff336724efd2426349c58a671
Lucas Lemos <lucas.lemos@aluno.ufca.edu.br>
2021-09-06 13:46:46 -0300

    Criado fxml e Classe de Controle para o funcionario

---
+++ Sistema com JavaFX/src/br/edu/ufca/controlador/ControleSistemaFuncionario.java
@@ -0,0 +1,83 @@
+package br.edu.ufca.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import br.edu.ufca.basicas.Arquivo;
+import br.edu.ufca.repositorio.RepositorioCliente;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ControleSistemaFuncionario implements Initializable{
+
+	private RepositorioCliente sistema = null;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.initialize();
+	}
+
+	@FXML
+    private void initialize() {
+		if(Arquivo.lerArquivo() != null) {
+			this.sistema = Arquivo.lerArquivo();
+		}
+    }
+
+	@FXML
+	Button btn01 = new Button();
+
+	@FXML
+	Button btn02 = new Button();
+
+	@FXML
+	Button btn03 = new Button();
+
+	@FXML
+	Button btn04 = new Button();
+
+	@FXML
+	Button btn05 = new Button();
+
+	@FXML
+	TextField tipoAnimal = new TextField();
+
+	@FXML
+	TextField qtdRacao = new TextField();
+
+	@FXML
+	Label qtdAnimais = new Label();
+
+	@FXML
+	public void func01() {
+		if(!tipoAnimal.getText().equals("")) {
+			String tipo = tipoAnimal.getText().toUpperCase();
+		}
+		else {
+			qtdAnimais.setText("Preencha o campo com o Tipo de Animal!");
+		}
+	}
+
+	@FXML
+	public void func02() {
+		
+	}
+
+	@FXML
+	public void func03() {
+		
+	}
+
+	@FXML
+	public void func04() {
+		
+	}
+
+	@FXML
+	public void func05() {
+		
+	}
+}
